5c76383bcbbe445813b122225ddf47c4c3c7d606
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-02-12 21:42:40 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ CMPS 2200  Recitation 2
 ### the only imports needed are here
 import tabulate
 import time
+import math
 ###
 
 def simple_work_calc(n, a, b):
@@ -17,7 +18,10 @@ def simple_work_calc(n, a, b):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
+	if n == 1:
+		return 1
+	else:
+		return a * simple_work_calc(n//b, a, b) + n
 	pass
 
 def work_calc(n, a, b, f):
@@ -27,12 +31,15 @@ def work_calc(n, a, b, f):
 	n......input integer
 	a......branching factor of recursion tree
 	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
+	f......a function that takes an integer and returns
+           the work done at each node
 
 	Returns: the value of W(n).
 	"""
-	# TODO
+	if n == 1:
+		return 1
+	else:
+		return a * work_calc(n//b, a, b, f) + f(n)
 	pass
 
 def span_calc(n, a, b, f):
@@ -42,34 +49,35 @@ def span_calc(n, a, b, f):
 	n......input integer
 	a......branching factor of recursion tree
 	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
+	f......a function that takes an integer and returns
+           the work done at each node
 
 	Returns: the value of W(n).
 	"""
-	# TODO
+	if n == 1:
+		return f(1)
+	else:
+		return max(span_calc(n // b, a, b, f) for _ in range(a)) + f(n)
+
 	pass
 
 
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
-	Compare the values of different recurrences for 
+	Compare the values of different recurrences for
 	given input sizes.
 
 	Returns:
 	A list of tuples of the form
 	(n, work_fn1(n), work_fn2(n), ...)
-	
+
 	"""
 	result = []
 	for n in sizes:
 		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
+		result.append((n,work_fn1(n),work_fn2(n)))
+
 	return result
 
 def print_results(results):
@@ -83,22 +91,17 @@ def print_results(results):
 
 def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
-	Compare the values of different recurrences for 
+	Compare the values of different recurrences for
 	given input sizes.
 
 	Returns:
 	A list of tuples of the form
 	(n, work_fn1(n), work_fn2(n), ...)
-	
+
 	"""
 	result = []
 	for n in sizes:
 		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			span_fn1,
-			span_fn2
-			))
-	return result
-	
+		result.append((n, span_fn1, span_fn2))
 
+	return result
--- test_main.py
+++ test_main.py
@@ -2,14 +2,22 @@ from main import *
 
 def test_simple_work():
 	""" done. """
-	assert simple_work_calc(10, 2, 2) == #TODO
-	assert simple_work_calc(20, 3, 2) == #TODO
-	assert simple_work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == 36
+	assert simple_work_calc(20, 3, 2) == 230
+	assert simple_work_calc(30, 4, 2) == 650
+	# 3 more cases
+	assert simple_work_calc(40, 2, 2) == 224
+	assert simple_work_calc(50, 2, 2) == 276
+	assert simple_work_calc(60, 3, 2) == 960
 
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert work_calc(10, 2, 2,lambda n: 1) == 15
+	assert work_calc(20, 1, 2, lambda n: n*n) == 530
+	assert work_calc(30, 3, 2, lambda n: n) == 300
+	# 3 more cases
+	assert work_calc(40, 4, 2, lambda n: n) == 1600
+	assert work_calc(50, 5, 2, lambda n: n) == 31250
+	assert work_calc(60, 6, 2, lambda n: n) == 129600
 
 
 def test_compare_work():
@@ -17,11 +25,43 @@ def test_compare_work():
 	# functions taht can be passed to compare_work
 
 	# create work_fn1
+	test1 = lambda n: work_calc(n, 2, 2, lambda n: n**.5)
+
 	# create work_fn2
-	res = compare_work(work_fn1, work_fn2)
+	test2 = lambda n: work_calc(n, 2, 2, lambda n: n)
 
-	print(res)
+	# 3rd test for question 4
+	test3 = lambda n: work_calc(n, 2, 2, lambda n: n**2)
 
-	
+	t1 = compare_work(test1, test2)
+	t2 = compare_work(test2, test3)
+
+	print("Comparing c < log_b a vs. c = log_b a")
+	print_results(t1)
+	print("Comparing c = log_b a vs. c > log_b a")
+	print_results(t2)
+
+	# It was VERY hard to figure out why things werent working properly, if these could be
+	#included in the next lab handout, it would be much appreciated.
+	assert t1 is not None
+	assert t2 is not None
+
+
 def test_compare_span():
-	# TODO
+
+	span1 = lambda n: span_calc(n, 2, 2, lambda n: math.log(n))
+
+	span2 = lambda n: span_calc(n, 2, 2, lambda n: n)
+
+	span3 = lambda n: span_calc(n, 2, 2, lambda n: 1)
+
+	c1 = compare_span(span1, span2)
+	c2 = compare_span(span2, span3)
+
+	print("Comparing Log^2 vs Log")
+	print_results(c1)
+	print("Comparing Log vs Linear")
+	print_results(c2)
+
+	assert c1 is not None
+	assert c2 is not None
